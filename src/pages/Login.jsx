b2732eadac56745c3651512c7bd560d597d96838
mohamedhany1231 <mohamedhany61@gmail.com>
2024-06-21 07:04:15 +0300

    fix login responsiveness

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,7 @@ import Loader from "../ui/Loader";
 import useUser from "../hooks/user/useUser";
 import { useNavigate } from "react-router-dom";
 import useLogin from "../hooks/user/useLogin";
+import { useWindowWidth } from "@react-hook/window-size";
 
 function Login() {
   const { user, isLoadingUser } = useUser();
@@ -18,33 +19,40 @@ function Login() {
     formState: { errors },
   } = useForm();
 
+  const width = useWindowWidth();
+  const isLargeScreen = width >= 1024;
+
   const { login, isLoading, response } = useLogin();
 
   return (
     <div className=" grid h-[100vh] grid-rows-[auto_1fr]  bg-main-900 text-[#fff]">
-      <div className=" grid grid-cols-2 ">
-        <div className="h-full py-20">
-          <img
-            src="/beautiful-alternative-energy-plant-with-solar-panels.jpg"
-            alt="solar panel img"
-            className=" h-full rounded-r-[3rem]    object-cover  brightness-75"
-          />
-        </div>
+      <div className=" grid grid-cols-1 overflow-auto lg:grid-cols-2 ">
+        {isLargeScreen && (
+          <div className="h-full py-20">
+            <img
+              src="/beautiful-alternative-energy-plant-with-solar-panels.jpg"
+              alt="solar panel img"
+              className=" h-full rounded-r-[3rem]    object-cover  brightness-75"
+            />
+          </div>
+        )}
 
         {isLoadingUser ? (
           <Loader />
         ) : (
           <form
-            className=" flex h-full flex-col px-[2%] py-20 "
+            className=" flex h-full flex-col px-[2%] py-8 lg:py-20 "
             onSubmit={handleSubmit(login)}
           >
-            <div className=" mt-10 flex grow flex-col gap-4 rounded-3xl  px-8 py-16  ">
-              <h3 className="   text-4xl font-bold  ">
-                <span className="mb-2 block text-7xl">Welcome back!</span>
+            <div className=" mt-10 flex grow flex-col gap-4 rounded-3xl  px-8  lg:py-16  ">
+              <h3 className="  text-2xl font-bold md:text-3xl lg:text-4xl  ">
+                <span className=" mb-4 block text-3xl sm:text-4xl md:text-5xl lg:mb-2  lg:text-7xl">
+                  Welcome back!
+                </span>
                 Please log in to access your solar panel dashboard.
               </h3>
 
-              <p className=" mb-8 text-lg tracking-wide text-gray-400">
+              <p className=" mb-8 text-base tracking-wide text-gray-400 md:text-lg">
                 Monitor, analyze, and optimize your solar panel system. Sign in
                 now.
               </p>
